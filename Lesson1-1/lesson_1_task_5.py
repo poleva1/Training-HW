a21b102ee25a907c0f5bedfd807311a0c8cc8576
poleva1 <poleva1@yandex.ru>
2024-10-26 10:24:00 +0300

    add changes lesson1

---
+++ Lesson1-1/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_num(number):
+    print(number, end='')
+
+
+print_num('8')
+print_num('8')
+print_num('0')
+print_num('0')
+print_num('5')
+print_num('5')
+print_num('5')
+print_num('3')
+print_num('5')
+print_num('3')
+print_num('5')
