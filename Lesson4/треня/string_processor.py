44f6c85f9f7e531e02aec38d40d3aeb357d84d84
poleva1 <poleva1@yandex.ru>
2024-10-28 16:38:03 +0300

    Delete Lesson4/треня directory
    error

--- Lesson4/треня/string_processor.py
+++
@@ -1,9 +0,0 @@
-class StringProcessor:
-    @staticmethod
-    def process(text: str) -> str:
-        if not text:
-            return "."
-        processed_text = text[0].upper() + text[1:]
-        if not processed_text.endswith("."):
-            processed_text += "."
-        return processed_text
