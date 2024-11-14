34c2c7cc0414d30e554dcadef95b63932bf7387a
poleva1 <poleva1@yandex.ru>
2024-11-15 00:15:42 +0300

    Delete Python/lesson_03 directory

--- Python/lesson_03/lesson_3_task_3.py
+++
@@ -1,7 +0,0 @@
-from mailing import Mailing
-from address import Address
-
-to_address = Address("194358", "СПб", "ул. Композиторов", "12", "10")
-from_address = Address("194355", "СПб", "ул. Хошимина", "1", "1")
-mailing = Mailing(to_address, from_address, "99", "123456789")
-print(mailing)
